5f7b0980404ad854130844442ce3fa23ff2b4512
soukadao <s0ukada025@gmail.com>
2025-11-14 09:24:25 +0900

    remove: issue

--- src/features/get-tissues/get-issues.ts
+++ src/features/get-tissues/get-issues.ts
@@ -1,18 +1,5 @@
 import { githubFetcher } from "../../shared/index.js";
 
-interface Issue {
-  id: number;
-  number: number;
-  title: string;
-  state: string;
-  body: string | null;
-  user: {
-    login: string;
-  };
-  created_at: string;
-  updated_at: string;
-}
-
 export async function getIssues() {
   const url =
     "https://api.github.com/repos/soukadao/github-actions-test/issues";
